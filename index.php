d757bdb722af9d352810dc20f06c2d5d712477de
Meaghan Bass <meaghan.bass@gmail.com>
2019-05-24 11:33:56 -0700

    inheritance

--- index.php
+++ index.php
@@ -12,6 +12,39 @@
     </form>
 
     <?php
+        // inheritance
+        class Chef {
+            function makeChicken(){
+                echo "The chef makes chicken <br>";
+            }
+            function makeSalad(){
+                echo "The chef makes salad <br>";
+            }
+            function makeSpecialDish(){
+                echo "The chef makes bbq ribs <br>";
+            }
+        }
+
+        class ItalianChef extends Chef {
+            function makePasta(){
+                echo "The chef makes pasta <br>";
+            }
+            // overridng function
+            function makeSpecialDish(){
+                echo "The chef makes chicken parm <br>";
+            }
+        }
+
+        $chef = new Chef();
+        $chef->makeSpecialDish();
+
+        $italianChef = new ItalianChef();
+        $italianChef->makeSpecialDish();
+        
+    ?>
+
+    <?php
+    /*
         class Movie {
             public $title;
             private $rating;
@@ -37,6 +70,7 @@
         $avengers = new Movie("Avengers", "PG-13");
                     // G, PG, PG-13, R, NR
         echo $avengers->getRating();
+    */
     ?>
 
     <?php
