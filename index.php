ef051b81d7216fcd2b7397fece6922182eb34953
Meaghan Bass <meaghan.bass@gmail.com>
2019-05-24 10:28:52 -0700

    contructors

--- index.php
+++ index.php
@@ -17,21 +17,34 @@
             var $title;
             var $author;
             var $pages;
+
+            function __construct($aTitle, $aAuthor, $aPages){
+                $this->title = $aTitle;
+                $this->author = $aAuthor;
+                $this->pages = $aPages;
+            }
         }
 
+        $book1 = new Book("Harry Potter", "JK Rowling", 400);
+        $book2 = new Book("Lord of the Rings", "Tolkein", 700);
+
+        echo $book2->author;
+
+        /*
         // object
-        $book1 = new Book;
+        $book1 = new Book();
         $book1->title = "Harry Potter";
         $book1->author = "JK Rowling";
         $book1->pages = 400;
 
-        $book2 = new Book;
+        $book2 = new Book("Tom");
         $book2->title = "Lord of the Rings";
         $book2->author = "Tolkein";
         $book2->pages = 700;
 
         echo $book2->title;
         echo $book2->author;
+        */
     ?>
 
     <?php
