0dd02a8269898cffbf4995cbc8cfba254df537a9
Meaghan Bass <meaghan.bass@gmail.com>
2019-05-24 11:14:12 -0700

    getters and setters

--- index.php
+++ index.php
@@ -12,6 +12,35 @@
     </form>
 
     <?php
+        class Movie {
+            public $title;
+            private $rating;
+
+            function __construct($title, $rating) {
+                $this->title = $title;
+                $this->setRating($rating);
+            }
+
+            function getRating(){
+                return $this->rating;
+            }
+
+            function setRating($rating){
+                if($rating == "G" || $rating == "PG" || $rating == "PG-13" || $rating == "R" || $rating == "NR") {
+                    $this->rating = $rating;
+                } else {
+                    $this->rating = "NR";
+                }
+            }
+        }
+
+        $avengers = new Movie("Avengers", "PG-13");
+                    // G, PG, PG-13, R, NR
+        echo $avengers->getRating();
+    ?>
+
+    <?php
+    /*
         class Student {
             var $name;
             var $major;
@@ -35,6 +64,7 @@
         $student2 = new Student("Pam", "Art", 3.6);
 
         echo $student2->hasHonors();
+    */
     ?>
 
     <?php
